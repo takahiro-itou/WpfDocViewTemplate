6dcfff4ea78500fb0ebf642cc81592c1f2631250
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-06-06 11:48:48 +0900

    リファクタリング

--- ViewVb/MainWindow.xaml.vb
+++ ViewVb/MainWindow.xaml.vb
@@ -9,28 +9,8 @@ Public Sub New()
 ''--------------------------------------------------------------------
     InitializeComponent()
     Me.m_model = New MySampleModel()
-    SampleControl1.ViewModel = New MySampleViewModel(m_model)
+    SampleControl1.ViewModel = New WpfControl.Sample.SampleViewModel(m_model)
 End Sub
 
-Private Sub RunSample(ByVal message As String)
-''--------------------------------------------------------------------
-''    サンプルコード
-''--------------------------------------------------------------------
-Dim outText As String
-
-    outText = $"入力した文字列は {message} です"
-    SampleControl1.OutputText = outText
-    MsgBox(outText, MsgBoxStyle.OkOnly)
-End Sub
-
-Private Sub SampleControl1_RunButtonClick(
-        ByVal sender As Object, ByVal e As EventArgs)
-''--------------------------------------------------------------------
-''    イベントハンドラ
-''
-''    ユーザーコントロールの「実行ボタンクリック」
-''--------------------------------------------------------------------
-    RunSample(SampleControl1.InputText)
-End Sub
 
 End Class
