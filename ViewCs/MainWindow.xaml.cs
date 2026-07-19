09d5e9666d4a61357fbbb7219d103d9f145eb51e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-19 21:45:39 +0900

    サンプルアプリケーションの C# 版を実装

---
+++ ViewCs/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+namespace  ViewCs  {
+
+public  partial class  MainWindow : Window
+{
+
+    //----------------------------------------------------------------
+    /**   デフォルトコンストラクタ。
+    **
+    **/
+    public  MainWindow()
+    {
+        InitializeComponent();
+        this.m_model = New MySampleModel();
+        this.SampleControl1.ViewModel =
+            new WpfControl.Sample.SampleViewModel(this.m_model);
+    }
+
+    private MySampleModel   m_model;
+}
+
+}   //  End of namespace  ViewCs
